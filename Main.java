7a28f62a163cb5c874b0dd323362984f3fe19b78
unknown <kewinpriv@gmail.com>
2024-05-04 12:13:49 +0200

    zad2

--- Main.java
+++ Main.java
@@ -1,14 +1,17 @@
+import company.implementations.EmailMessenger;
 import company_beans.Person;
 import company_exceptions.InvalidAgeException;
 
 public class Main {
     public static void main(String[] args) {
+        EmailMessenger emailMessenger = new EmailMessenger();
+        emailMessenger.sendMessage("To jest testowa wiadomość e-mail.");
         try {
-            Person person = new Person("John Doe", 140);
+            Person person = new Person("John", 30);
             System.out.println("Name: " + person.getName());
             System.out.println("Age: " + person.getAge());
         } catch (InvalidAgeException e) {
-            System.out.println(e.getMessage());
+            System.err.println("Invalid age: " + e.getMessage());
         }
     }
 }
